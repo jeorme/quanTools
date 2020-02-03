73de706552523c6295d44740ccfd43419a7ba295
jerpetit <Konchan14>
2020-02-03 10:40:48 +0100

    add : pricer window barrier

--- quantools/analyticsTools/analyticsTools.py
+++ quantools/analyticsTools/analyticsTools.py
@@ -2,6 +2,7 @@ import QuantLib as ql
 
 
 def yearFraction(startDate,endDate,basis):
+    factor = 1.0
     if basis == "ACT/365.FIXED":
         day_count =  ql.Actual365Fixed()
     if basis == "ACT/360":
@@ -14,9 +15,12 @@ def yearFraction(startDate,endDate,basis):
         day_count = ql.Business252()
     if basis == "ACT29EXC":
         day_count = ql.Actual365NoLeap()
+    if basis == "ACT/365.25":
+        day_count = ql.Actual365Fixed()
+        factor = 365/365.25
     else:
         day_count = ql.Actual365Fixed()
-    return day_count.yearFraction(startDate,endDate)
+    return day_count.yearFraction(startDate,endDate) * factor
 
 
 
