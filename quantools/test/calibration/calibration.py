acf590d06d447888fb460af52942f11c5e871e3f
jerome <jeorme@hotmail.fr>
2020-02-25 23:13:35 +0100

    add unitary test

--- quantools/test/calibration/calibration.py
+++ quantools/test/calibration/calibration.py
@@ -1,20 +1,20 @@
 import json
 from quantools.library.fxvolCalibration.fxVolGood import constructFXVolSurface
 import unittest
-
+import numpy as np
 class calibOneExpiryOneVol(unittest.TestCase):
 
     """Test case utilisé pour tester la calibration d'une expiry et d'un surface"""
 
     def test_choice(self):
-        """Test le fonctionnement de la fonction 'random.choice'."""
+        """Test le calibration Broker Strangle"""
         with open("calibration_fqp.json", "r") as file:
             data = json.load(file)
         surface = constructFXVolSurface(data)
-        with open("calibration_fqp.json", "r") as file:
-            output = json.load("output_calibration_fqp.json")
+        with open("output_calibration_fqp.json", "r") as file:
+            output = json.load(file)
         # Vérifie que 'elt' est dans 'liste'
-        self.assertEqual(surface, output)
+        np.testing.assert_allclose(surface, output,err_msg="WARNING : regression",atol =1e-6)
 
 if __name__ == "__main__":
     unittest.main()
