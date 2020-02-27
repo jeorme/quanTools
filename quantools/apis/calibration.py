3ad5a0b282bd0c8f54352ee2a384be2453f31cce
jerpetit <Konchan14>
2020-02-27 18:40:57 +0100

    support multiple expiry and multiple FX vol

--- quantools/apis/calibration.py
+++ quantools/apis/calibration.py
@@ -4,6 +4,7 @@ from flask_restplus import Namespace,Resource, fields
 from quantools.library.fxvolCalibration.fxVol import constructFXVolSurface
 from quantools.library.fxvolCalibration.outputRestService import outputFxVolCalibrated
 from quantools.model.calibmodel.calibrationModel import mdValues, mdDef
+from quantools.model.heston.hestonModel import inputs_heston, input_md, hestonOutput
 
 api = Namespace('calibration', 'FX vol')
 
@@ -11,7 +12,7 @@ api = Namespace('calibration', 'FX vol')
 @api.route('/fxvol')
 class FxoVol(Resource):
     @api.response(200,"Success")
-    @api.expect(api.model("example",{"asOfDate":fields.String,
+    @api.expect(api.model("fx vol calibration",{"asOfDate":fields.String(default="2011-02-02"),
     "marketDataDefinitions":fields.Raw(example=mdDef,type="json"),
           "marketData"   :   fields.Raw(example=mdValues,type="json")
                                      }))
@@ -29,13 +30,17 @@ class FxoVol(Resource):
 @api.route('/heston')
 class Heston(Resource):
     @api.response(200,"Success")
-    @api.expect(api.model("input of heston calibration",{"TBD" : fields.Float}))
+    @api.expect(api.model("heston calibration", {"calibrationDate": fields.String(default="2016-07-05"),
+                                      "marketDataSet": fields.String(default="DEFAULT"),
+                                      "marketDataProvider" : fields.String("PE_STORE_MDP"),
+                                      "inputs" : fields.Raw(example=inputs_heston, type="json"),
+                                      "marketData": fields.Raw(example=input_md, type="json")
+                                      }))
     def post(self):
         """
-        interpolation : expiry axis
-        :return: the interpolated value
+        Heston calibration mock
+        :return: the expoected output of the calibration heston
         """
-        content = request.get_json()
-        TBD = content["TBD"]
+
         # define model
-        return jsonify({"TBD":TBD})
+        return jsonify({"calibrationResults":hestonOutput})
--- quantools/model/calibmodel/calibrationModel.py
+++ quantools/model/calibmodel/calibrationModel.py
@@ -23,126 +23,270 @@ class calibrationSchema(Schema):
 
 
 mdDef = {
-    "fxVolatilities": [
-        {
-            "basis": "ACT/365",
-            "butterflyStrategy": "STRANGLE",
-            "calibrationType": "MID",
-            "currencyPairId": "EUR/GBP",
-            "id": "EURGBP_Volatility",
-            "domesticCurrencyId": "GBP",
-            "domesticDiscountId": "gbpDiscount",
-            "smileAxis" : ["DELTA_CALL","DELTA_PUT"],
-            "expiries": [
-                {
-                    "atmConvention": "FORWARD",
-                    "atmQuoteId": "FX_ATM_CALL_VOL_2M",
-                    "butterflyQuoteIds": [
-                        {
-                            "delta": 0.25,
-                            "quoteId": "FX_BF25_VOL_1M"
-                        },
-                        {
-                            "delta": 0.1,
-                            "quoteId": "FX_BF10_VOL_1M"
-                        }
-                    ],
-                    "deliveryDate": "2011-04-13",
-                    "deltaConvention": "FORWARD",
-                    "expiryDate": "2011-04-11",
-                    "riskReversalQuoteIds": [
-                        {
-                            "delta": 0.25,
-                            "quoteId": "FX_RR25_VOL_1M"
-                        },
-                        {
-                            "delta": 0.10,
-                            "quoteId": "FX_RR10_VOL_1M"
-                        }
-                    ]
-                }
+      "fxVolatilities": [
+      {
+        "basis": "ACT/365",
+        "smileAxis": [
+          "DELTA_CALL",
+          "DELTA_PUT"
+        ],
+        "butterflyStrategy": "STRANGLE",
+        "calibrationType": "MID",
+        "currencyPairId": "EUR/GBP",
+        "id": "EURGBP_Volatility",
+        "domesticCurrencyId": "GBP",
+        "domesticDiscountId": "gbpDiscount",
+        "expiries": [
+          {
+            "atmConvention": "FORWARD",
+            "atmQuoteId": "FX_ATM_CALL_VOL_2M",
+            "butterflyQuoteIds": [
+              {
+                "delta": 0.25,
+                "quoteId": "FX_BF25_VOL_1M"
+              },
+              {
+                "delta": 0.1,
+                "quoteId": "FX_BF10_VOL_1M"
+              }
             ],
-            "expiryExtrapolationMethod": "FLAT",
-            "expiryInterpolationMethod": "FLAT",
-            "expiryInterpolationVariable": "TIME_WEIGHTED_TOTAL_VARIANCE",
-            "foreignCurrencyId": "EUR",
-            "foreignDiscountId": "eurDiscount",
-            "fxEvents": {},
-            "premiumAdjusted": True,
-            "smileExtrapolationMethod": "CUBIC_SPLINE",
-            "smileInterpolationMethod": "CUBIC_SPLINE",
-            "smileInterpolationVariable": "DELTA_CALL",
-            "strategyConvention": "BROKER_STRANGLE",
-            "surfaceType": "BF_RR"
-        }
+            "deliveryDate": "2011-04-13",
+            "deltaConvention": "FORWARD",
+            "expiryDate": "2011-04-11",
+            "riskReversalQuoteIds": [
+              {
+                "delta": 0.25,
+                "quoteId": "FX_RR25_VOL_1M"
+              },
+              {
+                "delta": 0.1,
+                "quoteId": "FX_RR10_VOL_1M"
+              }
+            ]
+          },
+          {
+            "atmConvention": "FORWARD",
+            "atmQuoteId": "FX_ATM_CALL_VOL_3M",
+            "butterflyQuoteIds": [
+              {
+                "delta": 0.25,
+                "quoteId": "FX_BF25_VOL_3M"
+              },
+              {
+                "delta": 0.1,
+                "quoteId": "FX_BF10_VOL_3M"
+              }
+            ],
+            "deliveryDate": "2011-05-13",
+            "deltaConvention": "FORWARD",
+            "expiryDate": "2011-05-11",
+            "riskReversalQuoteIds": [
+              {
+                "delta": 0.25,
+                "quoteId": "FX_RR25_VOL_3M"
+              },
+              {
+                "delta": 0.1,
+                "quoteId": "FX_RR10_VOL_3M"
+              }
+            ]
+          }
+        ],
+        "expiryExtrapolationMethod": "FLAT",
+        "expiryInterpolationMethod": "FLAT",
+        "expiryInterpolationVariable": "TIME_WEIGHTED_TOTAL_VARIANCE",
+        "foreignCurrencyId": "EUR",
+        "foreignDiscountId": "eurDiscount",
+        "fxEvents": {},
+        "premiumAdjusted": True,
+        "smileExtrapolationMethod": "CUBIC_SPLINE",
+        "smileInterpolationMethod": "CUBIC_SPLINE",
+        "smileInterpolationVariable": "DELTA_CALL",
+        "strategyConvention": "BROKER_STRANGLE",
+        "surfaceType": "BF_RR"
+      },
+      {
+        "basis": "ACT/365",
+        "butterflyStrategy": "STRANGLE",
+        "calibrationType": "MID",
+        "currencyPairId": "EUR/USD",
+        "id": "EURUSD_Volatility",
+        "domesticCurrencyId": "USD",
+         "smileAxis": [
+          "STRIKE",
+          "DELTA_PUT"
+        ],
+        "domesticDiscountId": "usdDiscount",
+        "expiries": [
+          {
+            "atmConvention": "FORWARD",
+            "atmQuoteId": "FX_ATM_CALL_VOL_6M",
+            "butterflyQuoteIds": [
+              {
+                "delta": 0.25,
+                "quoteId": "FX_BF25_VOL_6M"
+              },
+              {
+                "delta": 0.1,
+                "quoteId": "FX_BF10_VOL_6M"
+              }
+            ],
+            "deliveryDate": "2011-10-13",
+            "deltaConvention": "FORWARD",
+            "expiryDate": "2011-10-11",
+            "riskReversalQuoteIds": [
+              {
+                "delta": 0.25,
+                "quoteId": "FX_RR25_VOL_6M"
+              },
+              {
+                "delta": 0.1,
+                "quoteId": "FX_RR10_VOL_6M"
+              }
+            ]
+          },
+          {
+            "atmConvention": "FORWARD",
+            "atmQuoteId": "FX_ATM_CALL_VOL_1Y",
+            "butterflyQuoteIds": [
+              {
+                "delta": 0.25,
+                "quoteId": "FX_BF25_VOL_1Y"
+              },
+              {
+                "delta": 0.1,
+                "quoteId": "FX_BF10_VOL_1Y"
+              }
+            ],
+            "deliveryDate": "2012-05-13",
+            "deltaConvention": "FORWARD",
+            "expiryDate": "2012-05-11",
+            "riskReversalQuoteIds": [
+              {
+                "delta": 0.25,
+                "quoteId": "FX_RR25_VOL_1Y"
+              },
+              {
+                "delta": 0.1,
+                "quoteId": "FX_RR10_VOL_1Y"
+              }
+            ]
+          }
+        ],
+        "expiryExtrapolationMethod": "FLAT",
+        "expiryInterpolationMethod": "FLAT",
+        "expiryInterpolationVariable": "TIME_WEIGHTED_TOTAL_VARIANCE",
+        "foreignCurrencyId": "EUR",
+        "foreignDiscountId": "eurDiscount",
+        "fxEvents": {},
+        "premiumAdjusted": True,
+        "smileExtrapolationMethod": "CUBIC_SPLINE",
+        "smileInterpolationMethod": "CUBIC_SPLINE",
+        "smileInterpolationVariable": "DELTA_CALL",
+        "strategyConvention": "BROKER_STRANGLE",
+        "surfaceType": "BF_RR"
+      }
     ],
     "yieldCurves": [
-        {
-            "currencyId": "EUR",
-            "extrapolationAfterLastPoint": {
-                "method": "LINEAR",
-                "variable": "ZERO_COUPON_RATE"
-            },
-            "extrapolationBeforeFirstPoint": {
-                "method": "LINEAR",
-                "variable": "ZERO_COUPON_RATE"
-            },
-            "interpolation": {
-                "method": "LINEAR",
-                "variable": "ZERO_COUPON_RATE"
-            },
-            "maturities": [
-                "2011-02-03",
-                "2011-02-11",
-                "2011-02-18",
-                "2011-02-25",
-                "2011-03-04",
-                "2011-04-04",
-                "2011-05-04",
-                "2011-06-07",
-                "2011-07-06",
-                "2011-08-05",
-                "2011-10-16",
-                "2012-01-18",
-                "2013-01-21"
-            ],
-            "id": "eurDiscount",
-            "zeroCouponBasis": "ACT/365",
-            "zeroCouponFormula": "EXPONENTIAL"
+      {
+        "currencyId": "EUR",
+        "extrapolationAfterLastPoint": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
         },
-        {
-            "currencyId": "GBP",
-            "extrapolationAfterLastPoint": {
-                "method": "LINEAR",
-                "variable": "ZERO_COUPON_RATE"
-            },
-            "extrapolationBeforeFirstPoint": {
-                "method": "LINEAR",
-                "variable": "ZERO_COUPON_RATE"
-            },
-            "interpolation": {
-                "method": "LINEAR",
-                "variable": "ZERO_COUPON_RATE"
-            },
-            "maturities": [
-                "2011-02-03",
-                "2011-02-11",
-                "2011-02-18",
-                "2011-02-25",
-                "2011-03-04",
-                "2011-04-04",
-                "2011-05-04",
-                "2011-06-07",
-                "2011-07-06",
-                "2011-08-05",
-                "2011-10-16",
-                "2012-01-18",
-                "2013-01-21"
-            ],
-            "id": "gbpDiscount",
-            "zeroCouponBasis": "ACT/365",
-            "zeroCouponFormula": "EXPONENTIAL"
-        }
+        "extrapolationBeforeFirstPoint": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
+        },
+        "interpolation": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
+        },
+        "maturities": [
+          "2011-02-03",
+          "2011-02-11",
+          "2011-02-18",
+          "2011-02-25",
+          "2011-03-04",
+          "2011-04-04",
+          "2011-05-04",
+          "2011-06-07",
+          "2011-07-06",
+          "2011-08-05",
+          "2011-10-16",
+          "2012-01-18",
+          "2013-01-21"
+        ],
+        "id": "eurDiscount",
+        "zeroCouponBasis": "ACT/365",
+        "zeroCouponFormula": "EXPONENTIAL"
+      },
+      {
+        "currencyId": "GBP",
+        "extrapolationAfterLastPoint": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
+        },
+        "extrapolationBeforeFirstPoint": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
+        },
+        "interpolation": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
+        },
+        "maturities": [
+          "2011-02-03",
+          "2011-02-11",
+          "2011-02-18",
+          "2011-02-25",
+          "2011-03-04",
+          "2011-04-04",
+          "2011-05-04",
+          "2011-06-07",
+          "2011-07-06",
+          "2011-08-05",
+          "2011-10-16",
+          "2012-01-18",
+          "2013-01-21"
+        ],
+        "id": "gbpDiscount",
+        "zeroCouponBasis": "ACT/365",
+        "zeroCouponFormula": "EXPONENTIAL"
+      },
+      {
+        "currencyId": "USD",
+        "extrapolationAfterLastPoint": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
+        },
+        "extrapolationBeforeFirstPoint": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
+        },
+        "interpolation": {
+          "method": "LINEAR",
+          "variable": "ZERO_COUPON_RATE"
+        },
+        "maturities": [
+          "2011-02-03",
+          "2011-02-11",
+          "2011-02-18",
+          "2011-02-25",
+          "2011-03-04",
+          "2011-04-04",
+          "2011-05-04",
+          "2011-06-07",
+          "2011-07-06",
+          "2011-08-05",
+          "2011-10-16",
+          "2012-01-18",
+          "2013-01-21"
+        ],
+        "id": "usdDiscount",
+        "zeroCouponBasis": "ACT/365",
+        "zeroCouponFormula": "EXPONENTIAL"
+      }
     ]
 }
 mdValues = {
@@ -182,12 +326,35 @@ mdValues = {
                 0.8947970522,
                 0.8674010426
             ]
+        },
+        {
+            "id": "usdDiscount",
+            "discountFactors": [
+                0.9999902779,
+                0.9999847224,
+                0.9999417527,
+                0.9998952873,
+                0.9998480099,
+                0.9997647742,
+                0.9995274386,
+                0.9992303035,
+                0.9642335076,
+                0.9440648339,
+                0.9206156551,
+                0.8947970522,
+                0.8674010426
+            ]
         }
     ],
     "fxRates": [
         {
             "currencyPairId": "EUR/GBP",
             "quoteValue": 0.8566,
+            "spotDate": "2011-02-04"
+        },
+        {
+            "currencyPairId": "EUR/USD",
+            "quoteValue": 1.12,
             "spotDate": "2011-02-04"
         }
     ],
@@ -198,7 +365,8 @@ mdValues = {
                 {
                     "quoteId": "FX_RR10_VOL_1M",
                     "value": 0.0015
-                }, {
+                },
+                {
                     "quoteId": "FX_RR25_VOL_1M",
                     "value": 0.001
                 },
@@ -213,6 +381,71 @@ mdValues = {
                 {
                     "quoteId": "FX_ATM_CALL_VOL_2M",
                     "value": 0.097
+                },
+                {
+                    "quoteId": "FX_RR10_VOL_3M",
+                    "value": 0.002
+                },
+                {
+                    "quoteId": "FX_RR25_VOL_3M",
+                    "value": 0.0015
+                },
+                {
+                    "quoteId": "FX_BF10_VOL_3M",
+                    "value": 0.00675
+                },
+                {
+                    "quoteId": "FX_BF25_VOL_3M",
+                    "value": 0.003
+                },
+                {
+                    "quoteId": "FX_ATM_CALL_VOL_3M",
+                    "value": 0.107
+                }
+            ]
+        },
+        {
+            "id": "EURUSD_Volatility",
+            "quotes": [
+                {
+                    "quoteId": "FX_RR10_VOL_6M",
+                    "value": 0.0015
+                },
+                {
+                    "quoteId": "FX_RR25_VOL_6M",
+                    "value": 0.001
+                },
+                {
+                    "quoteId": "FX_BF10_VOL_6M",
+                    "value": 0.00575
+                },
+                {
+                    "quoteId": "FX_BF25_VOL_6M",
+                    "value": 0.002
+                },
+                {
+                    "quoteId": "FX_ATM_CALL_VOL_6M",
+                    "value": 0.097
+                },
+                {
+                    "quoteId": "FX_RR10_VOL_1Y",
+                    "value": 0.002
+                },
+                {
+                    "quoteId": "FX_RR25_VOL_1Y",
+                    "value": 0.0015
+                },
+                {
+                    "quoteId": "FX_BF10_VOL_1Y",
+                    "value": 0.00675
+                },
+                {
+                    "quoteId": "FX_BF25_VOL_1Y",
+                    "value": 0.003
+                },
+                {
+                    "quoteId": "FX_ATM_CALL_VOL_1Y",
+                    "value": 0.107
                 }
             ]
         }
